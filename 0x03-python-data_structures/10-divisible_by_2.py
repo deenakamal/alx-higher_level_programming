33bc57f62c4e42adcb8dc0009ce59aaf713bef6d
deenakamal <deenah.kamal@gmail.com>
2023-12-03 14:49:23 -0800

    c

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+cp_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            cp_list.append(True)
+        else:
+            cp_list.append(False)
+
+    return cp_list
