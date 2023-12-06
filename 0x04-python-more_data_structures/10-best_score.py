857ab772dfdda8ccfc507a75b7c1fce1dc3d605c
deenakamal <deenah.kamal@gmail.com>
2023-12-06 15:48:54 -0800

    get max

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_val = 0
+    max_key = None
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value > max_val:
+                max_val = value
+                max_key = key
+        return max_key
+    else:
+        return None
