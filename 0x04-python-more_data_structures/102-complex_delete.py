58e972595b78b5e165fb184bee9c183d12f37735
deenakamal <deenah.kamal@gmail.com>
2023-12-06 17:19:15 -0800

    add

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@ def complex_delete(a_dictionary, value):
     to_delete = []
     for key, v in a_dictionary.items():
         if v == value:
-            to_delet.append(key)
+            to_delete.append(key)
     for key in to_delete:
         del a_dictionary[key]
     return a_dictionary
