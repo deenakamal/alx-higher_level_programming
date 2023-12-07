ebc78ab9ac16e1579aac503a6c47eee26b00d9bf
deenakamal <deenah.kamal@gmail.com>
2023-12-06 16:16:21 -0800

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,13 @@ def roman_to_int(roman_string):
         return 0
     result = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-    for idx in range(len(roman_string)):
+    idx = 0
+    while idx < len(roman_string):
         current_value = d[roman_string[idx]]
         if idx > 0 and current_value > d[roman_string[idx - 1]]:
-            result += current_value - d[roman_string[idx - 1]]
-            idx = idx - 1
+            result += current_value - 2 * d[roman_string[idx - 1]]
+
         else:
             result += current_value
+        idx += 1
     return result
