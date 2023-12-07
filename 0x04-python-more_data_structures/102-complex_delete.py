ffea93c708ba3277328da227c84547ae72c6d6f5
deenakamal <deenah.kamal@gmail.com>
2023-12-06 17:17:06 -0800

    complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    to_delete = []
+    for key, v in a_dictionary.items():
+        if v == value:
+            to_delet.append(key)
+    for key in to_delete:
+        del a_dictionary[key]
+    return a_dictionary
+
