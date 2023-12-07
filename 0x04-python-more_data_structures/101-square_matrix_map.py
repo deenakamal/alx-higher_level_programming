f899f5d396c1a010e189a1f5642b209e103e91d9
deenakamal <deenah.kamal@gmail.com>
2023-12-06 17:12:47 -0800

    update

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda m: list(map(lambda l: l*l, m)), matrix))
+    return list(map(lambda m: list(map(lambda f: f*f, m)), matrix))
