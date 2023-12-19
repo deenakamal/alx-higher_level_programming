10bb41bbf29aa0d79d6d4b71d427fe0a53dfb9cf
deenakamal <deenah.kamal@gmail.com>
2023-12-19 13:03:13 -0800

    commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class square """
+
+
+class Square:
+    """ implementation of square class """
+
+    def __init__(self, size=0):
+        """Initializes the square instance.
+
+        Args:
+            size: size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size
