d8ba55e714c310a2cf1d9f32031e9aa608100e1a
deenakamal <deenah.kamal@gmail.com>
2023-12-19 12:50:13 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" calss square """
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square instance.
+
+        Args:
+            size: defines the size of new square
+        """
+        self.size = size
