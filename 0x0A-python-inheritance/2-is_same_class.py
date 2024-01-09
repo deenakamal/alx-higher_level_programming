4496fdb071fc72c53a5cd28af62f0edb2cde70d3
deenakamal <deenah.kamal@gmail.com>
2024-01-08 16:47:50 -0800

    add answers

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defines is_same_class function """
+
+
+def is_same_class(obj, a_class):
+    """Determines if an object is exactly an instance of a class."""
+    return type(obj) == a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" inherits_from Module """
+
+
+def inherits_from(obj, a_class):
+    """ return if an object is a subclass of a class or not"""
+    return isinstance(obj, a_class)
