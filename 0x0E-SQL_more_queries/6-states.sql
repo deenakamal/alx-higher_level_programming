ca3328a2277a744ca2d84d5fcc8f1425a4799843
deenakamal <deenah.kamal@gmail.com>
2024-02-15 15:20:53 -0800

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
--- creates the database hbtn_0d_usa
+-- creates the database hbtn_0d_usa.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256)
 );
