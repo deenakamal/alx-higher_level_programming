cfabc894e9f504981c152299a0f656d74001b8b5
deenakamal <deenah.kamal@gmail.com>
2024-02-15 14:57:49 -0800

    edit task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- creates the database hbtn_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 CREATE USER IS NOT EXISTS "user_0d_2"@"localhots" IDENTIFIED BY "user_0d_2_pwd";
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
