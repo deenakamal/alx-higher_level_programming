f4c93a07dc5553a1aa7c3189c419063936625e11
deenakamal <deenah.kamal@gmail.com>
2024-02-15 12:28:39 -0800

    adv

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,10 @@
 -- converts hbtn_0c_0 database to UTF8.
+USE mysql;
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
