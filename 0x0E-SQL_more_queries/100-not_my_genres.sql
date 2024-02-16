8725afc2b4c979c0076ede7e3421c29f1c591d6b
deenakamal <deenah.kamal@gmail.com>
2024-02-15 17:36:12 -0800

    adv

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- uses the hbtn_0d_tvshows database to list all genres.
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+) AS DexterGenres ON tv_genres.id = DexterGenres.genre_id
+WHERE DexterGenres.genre_id IS NULL
+ORDER BY tv_genres.name ASC;
