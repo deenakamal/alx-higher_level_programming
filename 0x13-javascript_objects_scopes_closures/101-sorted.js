cc5dade4c16bc20b04ca206452a53ae4702591a6
deenakamal <deenah.kamal@gmail.com>
2024-03-12 16:20:31 -0700

    add

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-
 const dict = require('./101-data').dict;
 const mDict = {};
 for (const [key, value] of Object.entries(dict)) {
-  if (mewDict[value] === undefined) {
+  if (mDict[value] === undefined) {
     mDict[value] = [key];
   } else {
     mDict[value].push(key);
