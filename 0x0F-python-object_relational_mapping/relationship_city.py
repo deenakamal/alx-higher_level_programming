85e0cdce6a304beec522af0a0e1c129758c5620b
deenakamal <deenah.kamal@gmail.com>
2024-03-17 13:04:34 -0700

    add

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module desfines city class"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base, relationship
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Class inherts from Base"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" deines state class"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State inherts from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
