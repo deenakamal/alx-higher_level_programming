4f152361acb99555dfe3bba9f474d462a02fb733
deenakamal <deenah.kamal@gmail.com>
2024-03-27 23:00:59 +0000

    edit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,11 +3,11 @@
 
 
 def binary_search(list_of_integers, start, end):
-    """ performs the recursive binary search """
+    """Performs the recursive binary search."""
     if not list_of_integers:
         return None
     if start == end:
-            return list_of_integers[start]
+        return list_of_integers[start]
 
     mid = (start + end) // 2
     if list_of_integers[mid] < list_of_integers[mid + 1]:
@@ -15,8 +15,9 @@ def binary_search(list_of_integers, start, end):
     else:
         return binary_search(list_of_integers, start, mid)
 
+
 def find_peak(list_of_integers):
-    """Functon that Finds a peak in a list of unsorted integers"""
+    """Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
     return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
