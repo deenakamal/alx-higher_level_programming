cabafac408aa5cbecd95001fa9801222c23d7129
deenakamal <deenah.kamal@gmail.com>
2024-03-28 15:00:56 -0700

    edit file

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     data = urllib.parse.urlencode({"email": sys.argv[2]})
     data = data.encode('utf-8')
-    with urlopen(url, data) as response:
+    with urllib.request.urlopen(url, data) as response:
         body = response.read()
      print(body.decode("utf-8"))
