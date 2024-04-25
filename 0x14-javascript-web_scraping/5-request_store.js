0dcfe458a840a9c64394d0bbf11f78f45152965d
deenakamal <deenah.kamal@gmail.com>
2024-04-25 12:05:54 -0700

    edit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,8 +6,7 @@ const fs = require('fs');
 request(args[2], (error, response, body) => {
   if (error) {
     console.log(error);
-  }
-  else {
+  } else {
     fs.writeFile(args[3], body, 'utf-8', (error) => {
       if (error) {
         console.log(error);
